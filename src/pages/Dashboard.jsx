bec47341a0f4612bdfb7010774af2f287ddbca48
Muhammad Irsyadul Fikri <irsyadulfikri270418@gmail.com>
2023-10-27 22:50:16 +0700

    semoga terpush

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -51,34 +51,6 @@ const Dashboard = () => {
                 </div>
               </div>
             </div>
-            <div className="col-md-6 mb-3">
-              <div className="card bg-success text-white h-100">
-                <div className="card-body py-5">Seminar Hasil</div>
-                <div
-                  className="card-footer d-flex"
-                  onClick={() => handleViewDetails("/semhas")}
-                >
-                  View Details
-                  <span className="ms-auto">
-                    <i className="bi bi-chevron-right"></i>
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-6 mb-3">
-              <div className="card bg-danger text-white h-100">
-                <div className="card-body py-5">Sidang Akhir</div>
-                <div
-                  className="card-footer d-flex"
-                  onClick={() => handleViewDetails("/sidang")}
-                >
-                  View Details
-                  <span className="ms-auto">
-                    <i className="bi bi-chevron-right"></i>
-                  </span>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </Container>
